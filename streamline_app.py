c787bae2d9077c7178203d0d46a74ab01006e039
Lidia <109220177+lidiaeck@users.noreply.github.com>
2022-07-14 12:24:06 +0300

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title ('My Parents New Healty Dimer')
-
+streamlit.header ('Menu')
+streamlit.text ('Omega3 and Blueberry Oatmeal')
+streamlit.text ('Kale Spinach and Rocket smoothie')
+streamlit.text ('hard boilled eggs')
